b8edac736da69a6e4075909de341deb82697b462
tsundere <benlmartin.work@gmail.com>
2021-12-06 11:55:16 +0000

    Fixing base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-  base: 'waldo-vision.github.io/waldo.vision'
+  base: '/waldo.vision/',
+  plugins: [preact()]
 })
