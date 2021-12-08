c0c89b07dc1eb14987371d6153106ee9de8de6c7
tsundere <benlmartin.work@gmail.com>
2021-12-08 20:12:40 +0000

    Adding animation

--- src/app.tsx
+++ src/app.tsx
@@ -7,7 +7,7 @@ export const App = () => {
     <div>
       <div class="relative py-20 px-4 min-h-screen flex items-center justify-center bg-gray-900 overflow-hidden">
         <Background/>
-        <div class="absolute top-10 right-10 animate-fadeIn">
+        <div class="absolute top-10 right-10 animate-fadeInx">
           <a href={"https://www.youtube.com/watch?v=GOI9EkLsUm0"} target="_blank">
             <div class="flex items-center gap-2 px-5 py-3 rounded-2xl bg-gray-700 shadow-md hover:bg-gray-600 transition-colors duration-100">
               <span class="text-gray-200 text-center text-lg md:text-lg capitalize">
@@ -41,10 +41,10 @@ export const App = () => {
             <Button
             class=""
             href="https://docs.waldo.vision">
-              <svg class=" fill-current text-white h-5 w-5 " xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z"/><path d="M14 3v5h5M16 13H8M16 17H8M10 9H8"/></svg>
+              <svg class=" fill-current text-white h-5 w-5 " xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#1E252980" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V8l-6-6z"/><path d="M14 3v5h5M16 13H8M16 17H8M10 9H8"/></svg>
             </Button>
             <Button
-            class=" border-2 border-pink-400"
+            class=" border-2 border-pink-400 "
             href="https://github.com/orgs/waldo-vision/people"
             >
               <svg class=" fill-current text-pink-400 h-5 w-5 " role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub Sponsors</title><path d="M17.625 1.499c-2.32 0-4.354 1.203-5.625 3.03-1.271-1.827-3.305-3.03-5.625-3.03C3.129 1.499 0 4.253 0 8.249c0 4.275 3.068 7.847 5.828 10.227a33.14 33.14 0 0 0 5.616 3.876l.028.017.008.003-.001.003c.163.085.342.126.521.125.179.001.358-.041.521-.125l-.001-.003.008-.003.028-.017a33.14 33.14 0 0 0 5.616-3.876C20.932 16.096 24 12.524 24 8.249c0-3.996-3.129-6.75-6.375-6.75zm-.919 15.275a30.766 30.766 0 0 1-4.703 3.316l-.004-.002-.004.002a30.955 30.955 0 0 1-4.703-3.316c-2.677-2.307-5.047-5.298-5.047-8.523 0-2.754 2.121-4.5 4.125-4.5 2.06 0 3.914 1.479 4.544 3.684.143.495.596.797 1.086.796.49.001.943-.302 1.085-.796.63-2.205 2.484-3.684 4.544-3.684 2.004 0 4.125 1.746 4.125 4.5 0 3.225-2.37 6.216-5.048 8.523z"/></svg>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,7 @@ export const Button = (props: any) => (
   <a
     href={props.href}
     target="_blank"
-    class={`p-3 font-semibold rounded-full transition-all border-2${props.class}`}
+    class={`p-3 font-semibold rounded-full border-2 transition duration-100 animate-fadeIn ${props.class}`}
   >
     {props.children}
   </a>
