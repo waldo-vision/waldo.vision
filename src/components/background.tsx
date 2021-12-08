d701f2c1f86fa0c9c80b57d7b9c8ea810bffcc96
Ben L Martin <83928834+tsundDEV@users.noreply.github.com>
2021-12-08 20:55:46 +0000

    Bg animation (#1)
    * Messing around with animation
    
    * Adding animation
    
    Co-authored-by: asuyou <asuyou@users.noreply.github.com>
    Co-authored-by: Asyo <a0988@protonmail.com>

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,54 +1,52 @@
-import { useEffect } from "preact/hooks"
-
+import { useEffect } from "preact/hooks";
 
 // All credit goes to this video for the background idea
 // https://youtube.com/watch?v=rCWM27pxqWw
 // I am willing to change if the original owner would like
 export const Background = () => {
   useEffect(() => {
-
     const gridItems = document.querySelectorAll(".grid-background > div");
 
-    gridItems.forEach((item:any) => {
-      const delay = getRandomInt(9, 10);
+    gridItems.forEach((item: any) => {
+      const delay = getRandom(9, 12);
 
-      const duration = getRandomInt(4, 9);
+      const duration = getRandom(1.5, 2.5);
 
       item.style.animationDelay = `${delay}s`;
       item.style.animationDuration = `${duration}s`;
     });
 
-    function getRandomInt(min: number, max: number) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+    function getRandom(min: number, max: number) {
+      return Math.random() * (max - min + 1) + min;
     }
-  }, [])
+  }, []);
   return (
-  <div class={`absolute grid-background inset-0 grid gap-2 transform -skew-y-6 scale-125 grid-rows-6 grid-cols-12 blur-sm`}>
-    <div class="row-span-3 bg-gray-800 rounded animate-pulse"></div>
-    <div class="row-span-2 bg-gray-800 rounded animate-pulse"></div>
-    <div class="row-span-2 col-span-2 bg-gray-800 rounded animate-pulse"></div>
+    <div class="absolute grid-background inset-0 grid gap-2 transform -skew-y-6 scale-125 grid-rows-6 grid-cols-12 blur-sm">
+      <div class="row-span-3 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-2 row-span-2 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="row-span-2 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="row-span-2 col-gray-2 bg-gray-800 rounded motion-safe:animate-pulse"></div>
 
-    <div class="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-3 bg-gray-800 rounded animate-pulse"></div>
+      <div class="col-span-4 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-2 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-3 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-3 bg-gray-800 rounded motion-safe:animate-pulse"></div>
 
-    <div class="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-7 bg-gray-800 rounded animate-pulse"></div>
+      <div class="col-span-3 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-7 bg-gray-800 rounded motion-safe:animate-pulse"></div>
 
-    <div class="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+      <div class="col-span-2 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-3 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-5 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-2 bg-gray-800 rounded motion-safe:animate-pulse"></div>
 
-    <div class="col-span-6 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-3 bg-gray-800 rounded animate-pulse"></div>
+      <div class="col-span-6 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-3 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-3 bg-gray-800 rounded motion-safe:animate-pulse"></div>
 
-    <div class="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-    <div class="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-  </div>)
-}
+      <div class="col-span-4 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-3 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+      <div class="col-span-5 bg-gray-800 rounded motion-safe:animate-pulse"></div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,7 @@ module.exports = {
   },
   variants: {
     extend: {},
+    animation: ['motion-safe']
   },
   plugins: [],
 }
